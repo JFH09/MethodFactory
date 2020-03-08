9381a3e5bf8c1744dbac7709c6e292227d62543f
Julian <Julian@PC-1>
2020-03-07 20:36:46 -0500

    Ya Esta

---
+++ src/main/java/PedidoContado.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Julian
+ */
+public class PedidoContado extends Pedido{
+    
+    public PedidoContado(double importe){
+        super(importe);
+    }
+    public void paga(){
+        System.out.println("El pago del pedido por importe de : "+ importe + " se ha realizado. ");
+    }
+    
+    public boolean valida(){
+        return true;
+    }
+}
---
+++ src/main/java/Usuario.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Julian
+ */
+public class Usuario {
+    public static void main(String[] args){
+        Cliente cliente;
+        cliente = new ClienteContado();
+        cliente.nuevoPedido(2000.0);
+        cliente.nuevoPedido(10000.0);
+        cliente = new ClienteCredito();
+        cliente.nuevoPedido(2000.0);
+        cliente.nuevoPedido(10000.0);
+    }
+}
